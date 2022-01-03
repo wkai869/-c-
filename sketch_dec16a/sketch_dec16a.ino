0a0d06e994ef4073042474939fac041cc1e431f6
doogle <52986076+wkai869@users.noreply.github.com>
2022-01-03 11:47:28 +0800

    开发板led屏幕
    led屏幕

---
+++ sketch_dec16a/sketch_dec16a.ino
@@ -0,0 +1,18 @@
+#include<LiquidCrystal.h>
+ 
+// 创建lcd控制对象,并指定其引脚与Arduino控制板对应关系
+const int rs=12,en=11,d4=5,d5=4,d6=3,d7=2;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+ 
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.print("zhangyan,");
+}
+ 
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print("Happy birthday!!!");
+}
+ 
+ 
